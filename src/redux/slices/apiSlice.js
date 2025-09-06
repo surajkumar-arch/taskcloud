a2320878ae7d5b07ab452b44207b6cf281442835
Surajjaiswal <surajkumarjaiswal5470@gmail.com>
2025-09-07 00:14:30 +0530

    setupod

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "https://taskcloud1fullstack.onrender.com/api";
+const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
+
 
 const baseQuery = fetchBaseQuery({
   baseUrl: API_URL,
